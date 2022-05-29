573f52e87ffe6476708f85d066215f6f08c9e272
theuszastro <matheuszastrodev@hotmail.com>
2022-05-29 16:52:45 -0300

    changed lib

--- etanol/src/lib.rs
+++ etanol/src/lib.rs
@@ -2,8 +2,6 @@
 
 mod methods;
 
-// https://stackoverflow.com/questions/60396593/how-do-i-use-rusqlites-rowget-method-for-a-type-which-i-dont-know-at-compile
-
 pub use etanol_databases::{Database, ModelWhere};
 pub use etanol_utils::Env;
 
